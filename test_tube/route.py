129d5752321fe0e9ef4b0132f2baa3224256819e
k-o-s-h <nutsuki@outlook.jp>
2020-05-17 20:39:26 +0900

    static header

--- test_tube/route.py
+++ test_tube/route.py
@@ -29,4 +29,15 @@ class Route():
 class Static(Route):
 
     def __init__(self, status, content_type):
-        super().__init__(None, None, None, status = status, content_type=content_type)
+        self.__status = status
+        self.__ct = content_type
+        self.__header = Headers()
+
+    @property
+    def status_code(self):
+        return '{} {}'.format(self.__status, responses[self.__status])
+
+    @property
+    def headers(self):
+        self.__header.add_header('Content-type', self.__ct)
+        return self.__header.items()
